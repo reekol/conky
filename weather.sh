8086eb09bc901dc6b45e48efc9cddab3e4e107d6
ReeKol <cupuyc@gmail.com>
2019-07-14 23:15:04 +0300

    Hourly weather for current day

--- weather.sh
+++ weather.sh
@@ -10,7 +10,18 @@ do
   date=$(echo "$line" | cut -d'|' -f1)
   temp=$(echo "$line" | cut -d'|' -f2)
   hook=$(echo "$line" | cut -d'|' -f3)
+  echo -e "$date: $temp -> $hook"
+done < <(echo $data | jq '.forecasts[0].detailed.reports[] | "\(.localDate)_\(.timeslot) | \(.temperatureC)C | \(.enhancedWeatherDescription)" ')
+)
+echo "----------------------------------------------"
+(
+while IFS= read -r line
+do
+  line=$(echo $line | tr '"' ' ')
+  date=$(echo "$line" | cut -d'|' -f1)
+  temp=$(echo "$line" | cut -d'|' -f2)
+  hook=$(echo "$line" | cut -d'|' -f3)
   icons=$(echo $hook | sed 's/intervals//gi; s/and//g; s/\ a\ //g; s/Sunny/\xe2\x98\x80/gi; s/showers/\xe2\x98\x94/gi; s/thundery/\\U26c8/gi; s/gentle\ breeze/\\U1f32b/gi')
   echo -e "$date: $temp -> $hook"
-done < <(echo $data | jq '.forecasts[] | "\(.summary.report.localDate) | \(.summary.report.minTempC)C/\(.summary.report.maxTempC)C | \(.summary.report.enhancedWeatherDescription)"')
+done < <(echo $data | jq '.forecasts[] | "\(.summary.report.localDate)| \(.summary.report.minTempC)C/\(.summary.report.maxTempC)C | \(.summary.report.enhancedWeatherDescription)"')
 )
