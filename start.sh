1f4fe49ef92dabb2e5d301e9966daee880cbd691
ReeKol <cupuyc@gmail.com>
2019-07-15 19:50:45 +0300

    Start weather

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ conky -c ./.conkyrc &&
 conky -c ./.conky-dmesg &&
 conky -c ./.conky-more &&
 conky -c ./.conky-cal &&
-conky -c ./.conky-rings
+conky -c ./.conky-rings &&
+conky -c ./.conky-weather
