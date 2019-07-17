76356c006f7443916b1065df8fa121e90582a40b
reekol <cupuyc@gmail.com>
2019-07-17 12:20:47 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -11,7 +11,7 @@ print_res(){
 }
 
 res=$(print_res | sort -r | column -t)
-echo -e $res | grep -v -E '^\$\{color0\}(.*)(sofia)'
+echo -e $res | grep -v -E '^\$\{color0\}(.*)(sofia)' | column -t
 
 offline=$(echo -e $res | grep -v -E '^\$\{color0\}' | grep color1 | wc -l)
 #if [ $offline -gt '0' ]; then play /usr/share/sounds/KDE-Im-Connection-Lost.ogg ; fi
