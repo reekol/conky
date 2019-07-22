7bf680ad9c3f9dace0c57f1d65e0034cbd917776
ReeKol <cupuyc@gmail.com>
2019-07-22 20:32:54 +0300

    socat as a comment

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ sudo killall conky
 sed -i "s/wlp[0-9]s0/$(ls /sys/class/net | grep wlp)/gi" ./.conkyrc
 sed -i "s/enp[0-9]s0/$(ls /sys/class/net | grep enp)/gi" ./.conkyrc
 
+#GET /wifi HTTP/1.1
+#socat -vv TCP-LISTEN:1234,crlf,reuseaddr,fork SYSTEM:"echo HTTP/1.0 200; echo Content-Type\: text/plain; echo; bash ~/.conky/scan_arp.sh" &
+
 sleep 2
      conky -c ./.conkyrc &&
      conky -c ./.conky-dmesg &&
