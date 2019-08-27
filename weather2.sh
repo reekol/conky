0cb6da03fe204e73c5bd6576152d01f7d56b42d8
n.terziev <n.terziev@dopamine.bg>
2019-08-27 09:14:17 +0300

    Weather format - short

--- weather2.sh
+++ weather2.sh
@@ -1,7 +1,8 @@
 
 source $PWD/config.sh
-data=$(curl -s "https://www.yahoo.com/news/weather/$(getConfig c_weather_loc)" -H 'authority: www.yahoo.com' -H 'cache-control: max-age=0' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'sec-fetch-site: none' -H 'referer: https://www.google.com/' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9,bg-BG;q=0.8,bg;q=0.7,ru;q=0.6' -H 'cookie: weather=unit%3Dmetric; thamba=2; APID=UP7a1b1400-d568-11e8-9d1f-02c3d9916472; GUCS=ASYt3dSG; GUC=AQABAQFdRc5eMEIengRd&s=AQAAADHu-Pu-&g=XUSKGA; APIDTS=1564772988; B=6ipq01pdt4a8f&b=3&s=0d' --compressed) && echo $data > $PWD/cache/wtr.cache
+#data=$(curl -s "https://www.yahoo.com/news/weather/$(getConfig c_weather_loc)" -H 'authority: www.yahoo.com' -H 'cache-control: max-age=0' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-user: ?1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'sec-fetch-site: none' -H 'referer: https://www.google.com/' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9,bg-BG;q=0.8,bg;q=0.7,ru;q=0.6' -H 'cookie: weather=unit%3Dmetric; thamba=2; APID=UP7a1b1400-d568-11e8-9d1f-02c3d9916472; GUCS=ASYt3dSG; GUC=AQABAQFdRc5eMEIengRd&s=AQAAADHu-Pu-&g=XUSKGA; APIDTS=1564772988; B=6ipq01pdt4a8f&b=3&s=0d' --compressed) && echo $data > $PWD/cache/wtr.cache
 data=$(cat $PWD/cache/wtr.cache)
+format=$1
 
 getPos(){
     echo $(echo $1 | grep -b -o "$2" | cut -d':' -f1 | head -n $3 | tail -n1)
@@ -39,7 +40,7 @@ getToday(){
 
 
     ) | sed 's/<!--[^>]*-->/ /g' | sed 's/<[^>]*>//gi' | grep -v -e '^[[:space:]]*$' | tr '="' ' ' | awk '{
-        print "${goto 40}${voffset 6}${font Monospace:size=7}"$2$3" "$4$5" "$7" ${image "$8" -s 20x20 -p 0,"((NR-1) * 18)" -f 86400} "
+        print "${goto 40}${voffset 5}${font Monospace:size=7}"$2$3" "$4$5" "$7" ${image "$8" -s 20x20 -p 0,"((NR-1) * 18)" -f 86400} "
     }' | column -t
 }
 
@@ -54,7 +55,11 @@ getForecast(){
         local dropText=$(substr "$row" '<img ' 2 '</span>' 4 | sed 's/<[^>]*>//gi'  )
         local descr=$(echo "<i $(substr "$row" 'class="day-part' 1 '</div>' 2)" | sed 's/<!--[^>]*-->/ /g' | sed 's/<[^>]*>//gi')
         local temps=$(echo "< $(substr "$row" 'class="D(ib) Va(m) W(1/4) Ta(end)"' 1 'class="day-part' 1)>" | sed 's/<!--[^>]*-->//g' | sed 's/<[^>]*>/ /gi' )
-        echo '${goto 50}${voffset 6}${font Monospace:size=7}'$dropText'|'$day'|'$temps'|'$descr'${image '$imgCond' -s 20x20 -p 0,' $(( 435 + (i-1) * 18 ))' -f 86400} ${image '$imgDrop' -s 14x14 -p 25,'$(( 435 + (i-1) * 18 ))' -f 86400}'
+        if [ $format = "short" ]; then
+            echo '${goto 50}${voffset 5}${font Monospace:size=7}'$dropText'|'$day'|'$temps'${image '$imgCond' -s 20x20 -p 0,' $(( 435 + (i-1) * 18 ))' -f 86400} ${image '$imgDrop' -s 14x14 -p 25,'$(( 435 + (i-1) * 18 ))' -f 86400}'
+        else
+            echo '${goto 50}${voffset 5}${font Monospace:size=7}'$dropText'|'$day'|'$temps'|'$descr'${image '$imgCond' -s 20x20 -p 0,' $(( 435 + (i-1) * 18 ))' -f 86400} ${image '$imgDrop' -s 14x14 -p 25,'$(( 435 + (i-1) * 18 ))' -f 86400}'
+        fi
 #        echo  -e "\n$day - $temps\n$imgCond\n$imgDrop $dropText\n$descr"
     done
     ) | column -t -s '|'
@@ -73,6 +78,13 @@ getNow(){
     echo "\${goto 995}\${font Monospace:size=15}$now\${font}"
     echo '${image '$imgCond' -s 50x50 -p 990,500 -f 86400}'
 }
-getToday
-getForecast
-getNow
+
+if [ $format = "short" ]; then
+    getToday
+    getForecast
+    getNow
+else
+    getToday
+    getForecast
+    getNow
+fi
